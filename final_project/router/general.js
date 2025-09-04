9ac6bbe45795022669bfc35c8f7d051fe7a75eee
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2025-09-04 19:23:43 +0700

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,8 +65,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.send(JSON.stringify(books[isbn].reviews, null, 4));
+  } else {
+    res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
