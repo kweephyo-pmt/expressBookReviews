1a77efac5978559cb797d1e3af79e262a369256e
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2025-09-04 19:19:50 +0700

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -46,8 +46,21 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const bookKeys = Object.keys(books);
+  const matchingBooks = {};
+  
+  bookKeys.forEach(key => {
+    if (books[key].title === title) {
+      matchingBooks[key] = books[key];
+    }
+  });
+  
+  if (Object.keys(matchingBooks).length > 0) {
+    res.send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    res.status(404).json({message: "No books found with this title"});
+  }
 });
 
 //  Get book review
